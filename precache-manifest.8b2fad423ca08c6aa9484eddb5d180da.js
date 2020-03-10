3ad183dd4d51c0d91737df6b9544ea4e3fd73ef9
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2020-03-10 16:24:50 -0300

    Updates

---
+++ precache-manifest.8b2fad423ca08c6aa9484eddb5d180da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b09bd4a81e10ddbd73219258fcfb997",
+    "url": "/converter/index.html"
+  },
+  {
+    "revision": "3a4545c0055d029cb333",
+    "url": "/converter/static/js/2.a8017869.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/converter/static/js/2.a8017869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96348357da8213cee8f",
+    "url": "/converter/static/js/main.6eea2731.chunk.js"
+  },
+  {
+    "revision": "7b0f71a4662d04e14831",
+    "url": "/converter/static/js/runtime-main.3d5fb9f9.js"
+  },
+  {
+    "revision": "7bd0a97dd2f02c6253e29ba9b66cd923",
+    "url": "/converter/static/media/brasil.7bd0a97d.png"
+  }
+]);
